b740ef0cde512f76253d4b15d46d00bb7afc47c0
BrittanyNovak <brittanynicole0106@gmail.com>
2014-08-07 19:07:28 -0500

    trying to add submit

---
+++ application.js
@@ -0,0 +1,10 @@
+function checkForm() {
+	var v1, v2, v3, v4, v5, v6;
+		v1 = $("#one").val();
+		v2 = $("#two").val();
+		v3 = $("#three").val();
+		v4 = $("#four").val();
+		v5 = $("#five").val();
+		v6 = $("#six").val();
+		
+	
